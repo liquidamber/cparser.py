07eb898e272835d16a10fc88ab77f796c4e261d3
K. Ikegami <liquid@logos.ic.i.u-tokyo.ac.jp>
2010-12-17 05:29:56 +0900

    Implements cast expression

--- cparser.py
+++ cparser.py
@@ -56,6 +56,8 @@ class ParenExpr(UniExpr):
         return "(%s)" % str(self.a)
     def give_back(self):
         return [Token("(","")] + self.a.give_back() + [Token(")","")]
+    def child(self):
+        return self.a
 
 class CastExpr(UniExpr):
     def __init__(self, a, typename):
@@ -91,6 +93,16 @@ class ExprList(object):
             ret += [Token(",", "") , i.give_back()]
         return ret
 
+class TokenList(object):
+    def __init__(self):
+        self.list = []
+    def __str__(self):
+        return " ".join([str(x) for x in self.list])
+    def append(self,x):
+        self.list.append(x)
+    def give_back(self):
+        return [] + self.list
+
 class CParserSyntaxError(BaseException):
     pass
 
@@ -115,6 +127,23 @@ class CParser(object):
         if not len(self.rev):
             self.rev.append(self.get_tok())
         return self.rev[-1]
+
+    def parse_paren_match(self):
+        p = self.get_tok()
+        l = TokenList()
+        if p.get_type() != "(":
+            raise CParserSyntaxError, "Not OpenParen"+str(p)
+        l.append(p)
+        while True:
+            a = self.cur_tok()
+            if a.get_type() == "(":
+                x = self.parse_paren_match()
+                l.append(x)
+            else:
+                self.get_tok()
+                l.append(a)
+                if a.get_type() == ")":
+                    return l
 
     def parse_prim_expr(self):
         a = self.get_tok()
@@ -181,9 +210,12 @@ class CParser(object):
               atype == "-" or atype == "~" or atype == "!"):
             b = self.parse_cast_expr()
             return UniExpr(b, a)
-#Unsupport for sizeof(int)
         elif atype == "sizeof":
-            b = self.parse_unary_expr()
+            if self.cur_tok().get_type() != "(":
+                b = self.parse_unary_expr()
+            else:
+                #buggy typename parse
+                b = self.parse_paren_match()
             return UniExpr(b, a)
         else:
             self.rev_tok(a)
@@ -193,13 +225,15 @@ class CParser(object):
     def parse_cast_expr(self):
         a = self.cur_tok()
         if a.get_type == "(":
-            self.get_tok()
-            a = self.parse_type_name()
-            b = self.get_tok()
-            if b.get_type != ")":
-                raise CParserSyntaxError, "Paren Not Match"+str(b)
+            #buggy typename parse
+            # self.get_tok()
+            # a = self.parse_type_name()
+            # b = self.get_tok()
+            # if b.get_type != ")":
+            #     raise CParserSyntaxError, "Paren Not Match"+str(b)
+            a = self.parse_paren_match()
             b = self.parse_cast_expr()
-            return CastExpr(b, a)
+            return CastExpr(b, a.child())
         else:
             return self.parse_unary_expr()
 
@@ -333,11 +367,12 @@ class CParser(object):
             a.append(self.parse_assign_expr())
         return a
 
-def test():
+def test(string):
     # x = CParser(r'int x <<= (4 > 5 && 5 == k%2&3!=4 ? a[2] : f(++*ptr)), char * hoge = "TEST" "This\tis\ta\"Pen\n"')
-    x = CParser(r'y = fuga ? x==5 : (++y + 20) * 8 & (4 >> 2 | 3) && org--')
+    x = CParser(string)
     p = x.parse_expr()
     print p
 
 if __name__=="__main__":
-    test()
+    test(r'y = fuga ? x==5 : (++y + 20) * 8 & (4 >> 2 | 3) && org--')
+    test(r'y = fuga ? x==sizeof(int)+5 : (y++ + 20) * 8 & (4 >> 2 | 3) && --*(double *)org')
